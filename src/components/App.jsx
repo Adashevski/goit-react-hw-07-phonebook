ae11ba2361e6dd2c7e5f8cae72a407a55c0d7052
Adam Kulik <92215997+Adashevski@users.noreply.github.com>
2023-08-22 10:21:27 +0200

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from 'redux/store';
+import ContactForm from 'components/ContactForm/ContactForm';
+import ContactList from 'components/ContactList/ContactList';
+import Filter from 'components/Filter/Filter';
+
+import '../index.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Provider store={store}>
+      <div style={{ maxWidth: '600px', padding: '0 20px', margin: '0 auto' }}>
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+      </div>
+    </Provider>
   );
 };
+
+export default App;
